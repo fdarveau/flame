f21226bd02aedd422e1a84b15f10a28d109ed5c0
François Darveau <fdarveau@users.noreply.github.com>
2021-06-26 15:06:47 -0400

    Add app categories (#2)
    add app categories

--- client/src/components/Apps/AppForm/AppForm.tsx
+++ client/src/components/Apps/AppForm/AppForm.tsx
@@ -1,58 +1,137 @@
-import { useState, useEffect, useRef, ChangeEvent, SyntheticEvent } from 'react';
+import { ChangeEvent, Dispatch, Fragment, SetStateAction, SyntheticEvent, useEffect, useState } from 'react';
 import { connect } from 'react-redux';
-import { addApp, updateApp } from '../../../store/actions';
-import { App, NewApp } from '../../../interfaces';
 
-import classes from './AppForm.module.css';
-
-import ModalForm from '../../UI/Forms/ModalForm/ModalForm';
-import InputGroup from '../../UI/Forms/InputGroup/InputGroup';
+import { App, Category, GlobalState, NewApp, NewCategory, NewNotification } from '../../../interfaces';
+import {
+  addApp,
+  addAppCategory,
+  createNotification,
+  getAppCategories,
+  updateApp,
+  updateAppCategory,
+} from '../../../store/actions';
 import Button from '../../UI/Buttons/Button/Button';
-import axios from 'axios';
+import InputGroup from '../../UI/Forms/InputGroup/InputGroup';
+import ModalForm from '../../UI/Forms/ModalForm/ModalForm';
+import { ContentType } from '../Apps';
+import classes from './AppForm.module.css';
 
 interface ComponentProps {
   modalHandler: () => void;
-  addApp: (formData: NewApp | FormData) => any;
-  updateApp: (id: number, formData: NewApp) => any;
+  contentType: ContentType;
+  categories: Category[];
+  category?: Category;
   app?: App;
+  addAppCategory: (formData: NewCategory) => void;
+  addApp: (formData: NewApp | FormData) => void;
+  updateAppCategory: (id: number, formData: NewCategory) => void;
+  updateApp: (id: number, formData: NewApp | FormData, previousCategoryId: number) => void;
+  createNotification: (notification: NewNotification) => void;
 }
 
 const AppForm = (props: ComponentProps): JSX.Element => {
   const [useCustomIcon, toggleUseCustomIcon] = useState<boolean>(false);
   const [customIcon, setCustomIcon] = useState<File | null>(null);
-  const [formData, setFormData] = useState<NewApp>({
+  const [categoryData, setCategoryData] = useState<NewCategory>({
+    name: '',
+    type: 'apps'
+  })
+
+  const [appData, setAppData] = useState<NewApp>({
     name: '',
     url: '',
+    categoryId: -1,
     icon: ''
-  });
+  })
 
-  const inputRef = useRef<HTMLInputElement>(null);
-
+  // Load category data if provided for editing
   useEffect(() => {
-    if (inputRef.current) {
-      inputRef.current.focus();
+    if (props.category) {
+      setCategoryData({ name: props.category.name, type: props.category.type });
+    } else {
+      setCategoryData({ name: '', type: "apps" });
     }
-  }, [inputRef])
+  }, [props.category])
 
+  // Load app data if provided for editing
   useEffect(() => {
     if (props.app) {
-      setFormData({
+      setAppData({
         name: props.app.name,
         url: props.app.url,
+        categoryId: props.app.categoryId,
         icon: props.app.icon
       })
     } else {
-      setFormData({
+      setAppData({
         name: '',
         url: '',
+        categoryId: -1,
         icon: ''
       })
     }
   }, [props.app])
 
-  const inputChangeHandler = (e: ChangeEvent<HTMLInputElement>): void => {
-    setFormData({
-      ...formData,
+  const formSubmitHandler = (e: SyntheticEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+
+    if (!props.category && !props.app) {
+      // Add new
+      if (props.contentType === ContentType.category) {
+        // Add category
+        props.addAppCategory(categoryData);
+        setCategoryData({ name: '', type: 'apps' });
+      } else if (props.contentType === ContentType.app) {
+        // Add app
+        if (appData.categoryId === -1) {
+          props.createNotification({
+            title: 'Error',
+            message: 'Please select category'
+          })
+          return;
+        }
+        if (customIcon) {
+          const data = new FormData();
+          data.append('icon', customIcon);
+  
+          data.append('name', appData.name);
+          data.append('url', appData.url);
+  
+          props.addApp(data);
+        } else {
+          props.addApp(appData);
+        }
+        setAppData({
+          name: '',
+          url: '',
+          categoryId: appData.categoryId,
+          icon: ''
+        })
+      }
+    } else {
+      // Update
+      if (props.contentType === ContentType.category && props.category) {
+        // Update category
+        props.updateAppCategory(props.category.id, categoryData);
+        setCategoryData({ name: '', type: 'apps' });
+      } else if (props.contentType === ContentType.app && props.app) {
+        // Update app
+        props.updateApp(props.app.id, appData, props.app.categoryId);
+        setAppData({
+          name: '',
+          url: '',
+          categoryId: -1,
+          icon: ''
+        })
+      }
+
+      props.modalHandler();
+    }
+  }
+
+  const inputChangeHandler = (e: ChangeEvent<HTMLInputElement>, setDataFunction: Dispatch<SetStateAction<any>>, data: any): void => {
+    setDataFunction({
+      ...data,
       [e.target.name]: e.target.value
     })
   }
@@ -63,31 +142,25 @@ const AppForm = (props: ComponentProps): JSX.Element => {
     }
   }
 
-  const formSubmitHandler = (e: SyntheticEvent<HTMLFormElement>): void => {
-    e.preventDefault();
-
-    if (!props.app) {
-      if (customIcon) {
-        const data = new FormData();
-        data.append('icon', customIcon);
-
-        data.append('name', formData.name);
-        data.append('url', formData.url);
-
-        props.addApp(data);
-      } else {
-        props.addApp(formData);
-      }
-    } else {
-      props.updateApp(props.app.id, formData);
-      props.modalHandler();
-    }
-
-    setFormData({
-      name: '',
-      url: '',
-      icon: ''
+  const selectChangeHandler = (e: ChangeEvent<HTMLSelectElement>, setDataFunction: Dispatch<SetStateAction<any>>, data: any): void => {
+    setDataFunction({
+      ...data,
+      categoryId: parseInt(e.target.value)
     })
+  }
+
+  let button = <Button>Submit</Button>
+
+  if (!props.category && !props.app) {
+    if (props.contentType === ContentType.category) {
+      button = <Button>Add new category</Button>;
+    } else {
+      button = <Button>Add new app</Button>;
+    }
+  } else if (props.category) {
+    button = <Button>Update category</Button>
+  } else if (props.app) {
+    button = <Button>Update app</Button>
   }
 
   return (
@@ -95,91 +168,146 @@ const AppForm = (props: ComponentProps): JSX.Element => {
       modalHandler={props.modalHandler}
       formHandler={formSubmitHandler}
     >
-      <InputGroup>
-        <label htmlFor='name'>App Name</label>
-        <input
-          type='text'
-          name='name'
-          id='name'
-          placeholder='Bookstack'
-          required
-          value={formData.name}
-          onChange={(e) => inputChangeHandler(e)}
-          ref={inputRef}
-        />
-      </InputGroup>
-      <InputGroup>
-        <label htmlFor='url'>App URL</label>
-        <input
-          type='text'
-          name='url'
-          id='url'
-          placeholder='bookstack.example.com'
-          required
-          value={formData.url}
-          onChange={(e) => inputChangeHandler(e)}
-        />
-        <span>
-          <a
-            href='https://github.com/pawelmalak/flame#supported-url-formats-for-applications-and-bookmarks'
-            target='_blank'
-            rel='noreferrer'
-          >
-            {' '}Check supported URL formats
-          </a>
-        </span>
-      </InputGroup>
-      {!useCustomIcon
-        // use mdi icon
-        ? (<InputGroup>
-            <label htmlFor='icon'>App Icon</label>
-            <input
-              type='text'
-              name='icon'
-              id='icon'
-              placeholder='book-open-outline'
-              required
-              value={formData.icon}
-              onChange={(e) => inputChangeHandler(e)}
-            />
-            <span>
-              Use icon name from MDI. 
-              <a
-                href='https://materialdesignicons.com/'
-                target='blank'>
-                {' '}Click here for reference
-              </a>
-            </span>
-            <span
-              onClick={() => toggleUseCustomIcon(!useCustomIcon)}
-              className={classes.Switch}>
-              Switch to custom icon upload
-            </span>
-          </InputGroup>)
-        // upload custom icon
-        : (<InputGroup>
-            <label htmlFor='icon'>App Icon</label>
-            <input
-              type='file'
-              name='icon'
-              id='icon'
-              required
-              onChange={(e) => fileChangeHandler(e)}
-              accept='.jpg,.jpeg,.png'
-            />
-            <span
-              onClick={() => toggleUseCustomIcon(!useCustomIcon)}
-              className={classes.Switch}>
-              Switch to MDI
-            </span>
-          </InputGroup>)
+      {props.contentType === ContentType.category
+        ? (
+          <Fragment>
+            <InputGroup>
+              <label htmlFor='categoryName'>Category Name</label>
+              <input
+                type='text'
+                name='name'
+                id='categoryName'
+                placeholder='Social Media'
+                required
+                value={categoryData.name}
+                onChange={(e) => inputChangeHandler(e, setCategoryData, categoryData)}
+              />
+            </InputGroup>
+          </Fragment>
+        )
+        : (
+          <Fragment>
+            <InputGroup>
+              <label htmlFor='name'>App Name</label>
+              <input
+                type='text'
+                name='name'
+                id='name'
+                placeholder='Bookstack'
+                required
+                value={appData.name}
+                onChange={(e) => inputChangeHandler(e, setAppData, appData)}
+              />
+            </InputGroup>
+            <InputGroup>
+              <label htmlFor='url'>App URL</label>
+              <input
+                type='text'
+                name='url'
+                id='url'
+                placeholder='bookstack.example.com'
+                required
+                value={appData.url}
+                onChange={(e) => inputChangeHandler(e, setAppData, appData)}
+              />
+              <span>
+                <a
+                  href='https://github.com/pawelmalak/flame#supported-url-formats-for-applications-and-bookmarks'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  {' '}Check supported URL formats
+                </a>
+              </span>
+            </InputGroup>
+            <InputGroup>
+              <label htmlFor='categoryId'>App Category</label>
+              <select
+                name='categoryId'
+                id='categoryId'
+                required
+                onChange={(e) => selectChangeHandler(e, setAppData, appData)}
+                value={appData.categoryId}
+              >
+                <option value={-1}>Select category</option>
+                {props.categories.map((category: Category): JSX.Element => {
+                  return (
+                    <option
+                      key={category.id}
+                      value={category.id}
+                    >
+                      {category.name}
+                    </option>
+                  )
+                })}
+              </select>
+            </InputGroup>
+            {!useCustomIcon
+              // use mdi icon
+              ? (<InputGroup>
+                  <label htmlFor='icon'>App Icon</label>
+                  <input
+                    type='text'
+                    name='icon'
+                    id='icon'
+                    placeholder='book-open-outline'
+                    required
+                    value={appData.icon}
+                    onChange={(e) => inputChangeHandler(e, setAppData, appData)}
+                  />
+                  <span>
+                    Use icon name from MDI. 
+                    <a
+                      href='https://materialdesignicons.com/'
+                      target='blank'>
+                      {' '}Click here for reference
+                    </a>
+                  </span>
+                  <span
+                    onClick={() => toggleUseCustomIcon(!useCustomIcon)}
+                    className={classes.Switch}>
+                    Switch to custom icon upload
+                  </span>
+                </InputGroup>)
+              // upload custom icon
+              : (<InputGroup>
+                  <label htmlFor='icon'>App Icon</label>
+                  <input
+                    type='file'
+                    name='icon'
+                    id='icon'
+                    required
+                    onChange={(e) => fileChangeHandler(e)}
+                    accept='.jpg,.jpeg,.png'
+                  />
+                  <span
+                    onClick={() => toggleUseCustomIcon(!useCustomIcon)}
+                    className={classes.Switch}>
+                    Switch to MDI
+                  </span>
+                </InputGroup>)
+            }
+          </Fragment>
+        )
       }
-      {!props.app
-        ? <Button>Add new application</Button>
-        : <Button>Update application</Button>
-      }
+      {button}
     </ModalForm>
   )
 }
 
-export default connect(null, { addApp, updateApp })(AppForm);
+const mapStateToProps = (state: GlobalState) => {
+  return {
+    categories: state.app.categories
+  }
+}
+
+const dispatchMap = {
+  getAppCategories,
+  addAppCategory,
+  addApp,
+  updateAppCategory,
+  updateApp,
+  createNotification
+}
+
+export default connect(mapStateToProps, dispatchMap)(AppForm);
